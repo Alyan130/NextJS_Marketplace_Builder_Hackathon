58b5b952ac1aac7b08f0047b8d8b3ea6fe1a6685
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2025-03-05 14:46:54 +0500

    chatbot integrated

--- src/components/layout/middlebar.tsx
+++ src/components/layout/middlebar.tsx
@@ -14,7 +14,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { BsDropbox } from "react-icons/bs";
 
 export default function Header() {
   const cart = useAppSelector((state) => state.cart);
